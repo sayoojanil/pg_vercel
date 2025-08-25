138c2551a8112a0e47e944d6bcaed76d854bb89a
sayooj anil <sayoojbgmi@gmail.com>
2025-08-26 00:24:20 +0530

    s

--- project/src/components/RentPage.tsx
+++ project/src/components/RentPage.tsx
@@ -163,25 +163,6 @@ const RentPage: React.FC = () => {
           </div>
         </div>
 
-        <div className="bg-white overflow-hidden shadow rounded-lg">
-          <div className="p-5">
-            <div className="flex items-center">
-              <div className="flex-shrink-0">
-                <DollarSign className="h-8 w-8 text-blue-500" />
-              </div>
-              <div className="ml-5 w-0 flex-1">
-                <dl>
-                  <dt className="text-sm font-medium text-gray-500 truncate">
-                    Collected
-                  </dt>
-                  <dd className="text-2xl font-semibold text-gray-900">
-                    ₹{paidRent.toLocaleString()}
-                  </dd>
-                </dl>
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div className="bg-white overflow-hidden shadow rounded-lg">
           <div className="p-5">
